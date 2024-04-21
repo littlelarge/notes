8df4c4d1fdf85f6efa868067dc1fceb1192cdff7
littlelarge <githubandlab@gmail.com>
2024-04-21 23:29:55 +0600

    removed hexcolor package, add grey color

--- lib/presentation/core/colours/colours.dart
+++ lib/presentation/core/colours/colours.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:hexcolor/hexcolor.dart';
 
 class Colours {
   static const primary = Colors.teal;
   static const primaryWhite = Colors.white;
-  static final errorColor = HexColor('8B0000');
+  static const errorColor = Color(0xFF8B0000);
+  static const grey = Colors.grey;
 }
